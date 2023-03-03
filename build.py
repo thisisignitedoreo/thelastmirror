306ce8c8ca488e244fc19333f43fac9ac3ee2591
acid <97063750+thisisignitedoreo@users.noreply.github.com>
2023-03-03 22:32:31 +0300

    [TLM] heck the encoding

--- build.py
+++ build.py
@@ -1,1 +1,7 @@
-ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ
+from lib import build
+import json
+
+with open("index.json", "r") as f:
+    parsed = json.loads(f.read())
+
+build(parsed, True)
