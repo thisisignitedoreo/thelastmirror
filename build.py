0f7c6d53f2b448b7dadf5810f618ddf351de7cd8
acid <97063750+thisisignitedoreo@users.noreply.github.com>
2023-03-03 22:29:25 +0300

    [TLM] fix

--- build.py
+++ build.py
@@ -1,7 +1,1 @@
-from lib import build
-import json
-
-with open("index.json", "r") as f:
-    parsed = json.loads(f.read())
-
-build(parsed, True)
+ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ
--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 from lib import parse
 import json
 
-parsed = parse()
+parsed = parse(True)
 
 with open("index.json", "w") as f:
     f.write(json.dumps(parsed))
